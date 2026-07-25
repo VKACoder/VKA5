b3ffcd7e842b48d4c8e808c8563967a5797e39d6
Anirudh Ramkumar <110461428+VKACoder@users.noreply.github.com>
2026-07-25 20:44:13 +0530

    Update IF_TB to use output registers
    Refactor testbench to use output signals for control flow and target address.

--- RTL/Instr_Fetch/IF_TB.sv
+++ RTL/Instr_Fetch/IF_TB.sv
@@ -7,18 +7,18 @@ module IF_TB();
 
     //Input to IF
     reg clk, rstn;
-    reg is_ctrl_flow, is_compressed_instr;
-    reg [63:0] target_addr;
+    reg is_ctrl_flow_o, is_compressed_instr_o;
+    reg [63:0] target_addr_o;
     
     //Output from IF
-    wire [63:0] current_pc, next_pc;
+    wire [63:0] current_pc_i;
     
     //DUT Instantiation
     IF DUT(
         .clk(clk), .rstn(rstn),
-        .is_ctrl_flow(is_ctrl_flow), .is_compressed_instr(is_compressed_instr),
-        .target_addr(target_addr),
-        .current_pc(current_pc), .next_pc(next_pc) );
+        .is_ctrl_flow_o(is_ctrl_flow_i), .is_compressed_instr_o(is_compressed_instr_i),
+        .target_addr_o(target_addr_i),
+        .current_pc_i(current_pc_o) );
     
     //Clk generation
     always #5 clk = ~clk;
@@ -29,71 +29,26 @@ module IF_TB();
         @(posedge clk) rstn = 1'b 0;
         repeat (2) @(posedge clk);
         rstn = 1'b 1;
-        target_addr = 64'd 0;
-        is_ctrl_flow = 1'b 0;
-        is_compressed_instr = 1'b 0;
+        target_addr_o = 64'd 0;
+        is_ctrl_flow_o = 1'b 0;
+        is_compressed_instr_o = 1'b 0;
     end
     
     initial begin
         repeat (10) @(posedge clk);
         for (int i = 0; i < 20; i++) begin
             @(posedge clk)
-            target_addr = {$urandom(), $urandom()};
-            {is_ctrl_flow, is_compressed_instr} = $urandom();
+            target_addr_o = {$urandom(), $urandom()};
+            {is_ctrl_flow_o, is_compressed_instr_o} = $urandom();
         end
         @(posedge clk)
-        target_addr = 64'd 0;
-        is_ctrl_flow = 1'b 0;
-        is_compressed_instr = 1'b 0;
+        target_addr_o = 64'd 0;
+        is_ctrl_flow_o = 1'b 0;
+        is_compressed_instr_o = 1'b 0;
     end
     
     initial begin
         repeat (40) @(posedge clk);
-        $finish;
-    end
-
-endmodule
-`timescale 1ns / 1ps
-
-module IF_TB();
-
-    //import uvm_pkg::*;
-    //`include "uvm_macros.svh"
-
-    //Input to IF
-    reg clk, rstn;
-    reg is_ctrl_flow, is_compressed_instr;
-    reg [63:0] target_addr;
-    
-    //Output from IF
-    wire [63:0] current_pc, next_pc;
-    
-    //DUT Instantiation
-    IF DUT(
-        .clk(clk), .rstn(rstn),
-        .is_ctrl_flow(is_ctrl_flow), .is_compressed_instr(is_compressed_instr),
-        .target_addr(target_addr),
-        .current_pc(current_pc), .next_pc(next_pc) );
-    
-    //Clk generation
-    always #5 clk = ~clk;
-    
-    initial begin
-        clk = 1'b 0;
-        rstn = 1'b 1;
-        @(posedge clk) rstn = 1'b 0;
-        repeat (2) @(posedge clk);
-        rstn = 1'b 1;
-    end
-    
-    initial begin
-        is_compressed_instr = 1'b 0;
-        is_ctrl_flow = 1'b 0;
-        target_addr = 64'd 10;
-    end
-    
-    initial begin
-        repeat (20) @(posedge clk);
         $finish;
     end
 
