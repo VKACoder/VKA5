f2262946be7b41d6fab35c07a0209204e11daa68
Anirudh Ramkumar <110461428+VKACoder@users.noreply.github.com>
2026-07-25 20:42:31 +0530

    Refactor inputs and outputs in IF.sv module

--- RTL/Instr_Fetch/IF.sv
+++ RTL/Instr_Fetch/IF.sv
@@ -4,43 +4,45 @@ module IF
 )
 (
     clk, rstn,
-    is_ctrl_flow, is_compressed_instr,
-    target_addr,
-    current_pc, next_pc
+    is_ctrl_flow_i, is_compressed_instr_i,
+    target_addr_i,
+    current_pc_o
     
 );
 
     //Inputs
     input clk, rstn;
-    input is_ctrl_flow, is_compressed_instr;
-    input [63:0] target_addr;
+    input is_ctrl_flow_i, is_compressed_instr_i;
+    input [63:0] target_addr_i;
     
     //Outputs
-    output reg [63:0] current_pc;
-    output reg [63:0] next_pc;
+    output reg [63:0] current_pc_o;
+    
+    //Reg declaration
+    reg [63:0] next_pc;
 
     always_comb begin : next_pc_logic
         if (rstn == 1'b 0) begin
             next_pc = boot_addr;
         end
         else begin
-            if (is_compressed_instr)
-                next_pc = current_pc + 64'h 2;
+            if (is_compressed_instr_i)
+                next_pc = current_pc_o + 64'h 2;
             else
-                next_pc = current_pc + 64'h 4; 
+                next_pc = current_pc_o + 64'h 4; 
         end
     end
 
     always_ff @ (posedge clk) begin : current_pc_logic
         if (rstn == 1'b 0) begin
-            current_pc <= boot_addr;
+            current_pc_o <= boot_addr;
         end
         else begin
-            if (is_ctrl_flow) begin
-                current_pc <= target_addr;
+            if (is_ctrl_flow_i) begin
+                current_pc_o <= target_addr_i;
             end
             else begin
-                current_pc <= next_pc;
+                current_pc_o <= next_pc;
             end
         end
     end
