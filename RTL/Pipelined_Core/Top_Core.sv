3cec2cddcfaf2d67a0fc05e2da5e77690e1a522e
Anirudh Ramkumar <110461428+VKACoder@users.noreply.github.com>
2026-07-27 20:08:54 +0530

    Refactor Top_Core module inputs and outputs

--- RTL/Pipelined_Core/Top_Core.sv
+++ RTL/Pipelined_Core/Top_Core.sv
@@ -2,59 +2,61 @@ import RV64_pkg::*;
 
 module Top_Core(
     clk, rstn,
-    instr_fetched_i,
-    instr_fetched_valid,
-    pc_o
+    instr_fetched,
+    pc
     );
     
     //Inputs
     input clk, rstn;
-    input [31:0] instr_fetched_i;
-    input instr_fetched_valid;
+    input [31:0] instr_fetched;
     
     //Outputs
-    output [63:0] pc_o;
+    output [63:0] pc;
     
-    //Reg declarations
-    //IF/ID pipeline registers
+    //reg declaration
+    
+    
+    //wire declaration
+    wire is_compressed_instr;
+    wire Rd_en1, Rd_en2, Imm_valid;
+    wire [4:0] Reg_addr1, Reg_addr2;
+    wire [63:0] Rdata1, Rdata2;
+    wire [63:0] Imm_value;
+    wire [63:0] curr_pc_from_ID;
+    operarion op;
+    
+    //Pipeline registers
+    //IF/ID Stage
     reg [63:0] IF_current_pc;
-    reg [63:0] IF_instr;
+    reg [31:0] IF_instr;
+     
+    //ID/EX Stage
     
-    //ID/EX pipeline registers
-    operation ID_op;
-    reg [63:0] ID_current_pc;
-    reg [4:0] ID_rd_value;
-    reg [63:0] ID_rdata1, ID_rdata2, ID_imm_value;
-    
-    //Wire declarations
-    //ID Stage
-    wire [63:0] rdata1, rdata2;
-    wire is_valid_instr_i;
-    wire imm_value;
-    
-    
-    //Pipeline stages
+    //Pipeline stage instantiations
+    //IF STage
     IF IF_Stage(
         .clk(clk), .rstn(rstn),
-        .is_ctrl_flow_i(1'b 0), .is_compressed_instr_i(is_compressed_instr_i),
+        .is_ctrl_flow_i(1'b 0), .is_compressed_instr_i(is_compressed_instr),
         .target_addr_i(64'd 0),
-        .current_pc_o(pc_o) );
+        .current_pc_o(pc) );
         
-   ID ID_Stage(
+    //ID Stage
+    ID ID_Stage(
         .clk(clk), .rstn(rstn),
         .instr_fetched_i(IF_instr),
         .current_pc_i(IF_current_pc),
-        .is_compressed_instr_o(is_compressed_instr_i), .is_valid_instr_o(is_valid_instr_i),
-        .rd_en1(rd_en1), .rd_en2(rd_en2), .imm_valid(imm_valid),
-        .reg_addr1(reg_addr1), .reg_addr2(reg_addr2),
-        .imm_value_o(imm_value), 
-        .current_pc_i(current_pc_o),
-        .op_o(op_i) );
-        
-   RegFile_32x64 RegFile(.clk(clk), .rstn(rstn),
-                     .reg_addr1(reg_addr1), .reg_addr2(reg_addr2), 
-                     .wr_en(1'b 0), .rd_en1(rd_en1), .rd_en2(rd_en2),
-                     .wdata(64'd 0), .rdata1(rdata1), .rdata2(rdata2 ));
+        .is_compressed_instr_o(is_compressed_instr), .is_valid_instr_o(/*unconnected*/),
+        .rd_en1(Rd_en1), .rd_en2(Rd_en2), .imm_valid(Imm_valid),
+        .reg_addr1(Reg_addr1), .reg_addr2(Reg_addr2),
+        .imm_value_o(Imm_value),
+        .current_pc_o(curr_pc_from_ID),
+        .op_o(op) );
     
+    //Register File
+    RegFile_32x64 RegFile(
+        .clk(clk), .rstn(rstn),
+        .reg_addr1(Reg_addr1), .reg_addr2(Reg_addr2),
+        .wr_en(1'b 0), .rd_en1(Rd_en1), .rd_en2(Rd_en2),
+        .wdata(64'd 0), .rdata1(Rdata1), .rdata2(Rdata2) );    
     
 endmodule
